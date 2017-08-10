96e7430fafbb9a7302d98dde678c00131c883bd9
StayinNasty <keganswensen@gmail.com>
2017-08-09 21:08:19 -0600

    Still cant get finder to work

--- arraysPractice.js
+++ arraysPractice.js
@@ -100,13 +100,17 @@ var getRandomArbitrary = function() {
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
  function finder(getRandomArbitrary, arr){
-   for(var i = 0; i < arr.length; i++){
-     if(arr[i] === getRandomArbitrary()){
-       return true;
-     } else {
-       return false;
-     }
+  for(var i = 0; i < numbers.length; i++){
+     if(numbers[i] === getRandomArbitrary){
+      console.log(true); 
+      return true;
+      break;
+     } 
    }
+    if(i === numbers.length){
+      console.log(false);
+      return false;
+    }
  }
 
 
@@ -117,7 +121,9 @@ var getRandomArbitrary = function() {
 var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
 
-  //Code Here
+  function reverse (str){
+    return str.split('').reverse().join('');
+  }
 
 
 //Next Problem
@@ -139,7 +145,19 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   In both the removeItem function and the addItem function, you will also need to check for valid aurguments. Specrunner will try to call your functions without passing in valid aurguments. When this happens, you will need to respond by returning an empty array.
 */
 
-  //Code Here
+  function removeItem(myGroceryList, item){
+    if(myGroceryList.indexOf(item) !== -1){
+      myGroceryList.splice(myGroceryList.indexOf(item), 1);
+    }
+    return myGroceryList;
+  }
+
+  function addItem(myGroceryList, item){
+    if(myGroceryList.indexOf(item) === -1){
+      myGroceryList.push(item);
+    }
+    return myGroceryList;
+  }
 
 //removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
@@ -150,4 +168,161 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
 
 
-//Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the a
+//Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array with numbers
+//from 1 to 215, then returns the array.
+
+  function maker(){
+    var arr = [];
+    for(var i = 1; i <= 215; i++){
+      arr.push(i);
+    }
+    return arr;
+  }
+
+
+
+//Next Problem
+
+
+var numbers = [5, '9', 16, 19, '25', '34', 48];
+//Write a function called addTen that is given 'numbers' as it's only argument and returns a new
+//array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
+
+ function addTen(numbers){
+   var newArr = numbers.map(function(val){
+     return Number(val) + 10;
+   });
+    return newArr;
+ }
+
+
+
+//Next Problem
+
+
+
+var num1 = Math.floor(Math.random() * 30);
+var num2 = Math.floor(Math.random() * 30);
+var arr1 = [];
+var arr2 = [];
+for(var i = 0; i < num1; i++){
+  arr1.push(i);
+}
+for(var i = 0; i < num2; i++){
+  arr2.push(i);
+}
+//Above is some code that adds a random number of values to both arr1 and arr2.
+//Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
+
+  function longer(arr1, arr2){
+    if(arr1.length > arr2.length){
+      return arr1;
+    } else {
+      return arr2;
+    }
+  }
+
+
+/*As a continuation of the previous problem, write another function called 'both'.
+  Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example).
+  'both' should return a new array with the matching numbers found in both arr1 and arr2.
+  Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
+*/
+
+  function both(arr1, arr2){
+    var newArr = [];
+    for(var i = 0; i < arr1.length; i++){
+      if(arr2.indexOf(arr1[i]) !== -1){
+        newArr.push(arr1[i]);
+      }
+    }
+    return newArr;
+  }
+
+
+
+
+//NEXT PROBLEM
+
+
+
+
+var devMountainEmployees = [];
+
+var tyler = {
+    name: 'Tyler',
+    position: 'Lead Instructor/Engineer',
+    spiritAnimal: 'Honey Badger'
+};
+
+var cahlan = {
+    name: 'Cahlan',
+    position: 'CEO',
+    spiritAnimal: 'butterfly'
+};
+
+var ryan = {
+    name: 'Ryan',
+    position: 'Marketing',
+    spiritAnimal: 'fox'
+};
+
+var colt = {
+    name: 'Colt',
+    position: 'Everything really',
+    spiritAnimal: 'Young Male Horse'
+};
+
+/*Above you're given an empty array with four objects. Fill the devMountainEmployees
+array with those four objects. After that console.log the length of the Array and make
+sure that it's equal to 4. */
+
+  devMountainEmployees.push(tyler, cahlan, ryan, colt); 
+  console.log(devMountainEmployees.length); 
+
+/*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
+Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
+
+  for(var i = 0; i < devMountainEmployees.length; i++){
+    if(devMountainEmployees[i] === cahlan){
+      devMountainEmployees.splice(devMountainEmployees.indexOf(cahlan), 1);
+    }
+  }
+
+
+
+
+//NEXT PROBLEM
+
+
+/*A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects)
+of Data is to have an Array full of objects. */
+
+//Create an empty array called users.
+
+  var users = [];
+
+/*Now add three user objects to your users array. Each user object should contain the
+following properties. name, email, password, username.*/
+
+//include this as one of the objects in your array.
+var user1 = {
+    name: 'Tyler McGinnis',
+    email: 'tylermcginnis33@gmail.com',
+    password: 'iLoveJavaScript',
+    username: 'infiniateLoop'
+};
+
+//Your Code Here
+
+/*Now you have a very common data structure. Twitter is a good use case.
+It's easy to imagine that your followers list on Twitter is an Array full or objects
+and those objects contain properties about the specific person you follow.*/
+
+/*Now let's say that Tyler decided to delete his account. Loop through your array of
+objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
+Once you find the particular index he's located in, delete him from the array.*/
+
+  //Code Here
+
+//The activity we just did is very much how data works in 'the real world'.
